f8361461f69022b1c39c94d567f1c767d4389797
Louis D'Hont <xevrobelgium@gmail.com>
2021-12-02 21:50:48 +0100

    Added the basic layout and navbar & a logbook

---
+++ stackshare/src/lib/Navbar.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {page, session} from '$app/stores';
+</script>
+
+<nav class="navbar navbar-light">
+    <div class="container">
+        <a rel="prefetch" class="navbar-brand" href="/">StackShare</a>
+        <ul class="nav navbar-nav pull-xs-right">
+            <li class="nav-item">
+                <a rel="prefetch" class="nav-link" class:active={$page.path === '/'} href="/">Home</a>
+            </li>
+        </ul>
+    </div>
+</nav>
