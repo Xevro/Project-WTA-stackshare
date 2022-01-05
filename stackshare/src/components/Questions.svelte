9d756e81003df64ea692383bbb47ea647ebe701c
Louis D'Hont <xevrobelgium@gmail.com>
2022-01-05 02:21:00 +0100

    Added a search feature

--- stackshare/src/components/Questions.svelte
+++ stackshare/src/components/Questions.svelte
@@ -5,20 +5,37 @@
     let questions: Questions;
     let loading = true;
     let error = '';
+    let searchValue = null;
+    let filteredQuestions: Questions;
     const questionsProxy = new QuestionsProxyService();
 
-    questionsProxy.getAllQuestions().then(response => response.json())
-        .then((response: Questions) => {
-            questions = response;
+    const getQuestions = () => {
+        questionsProxy.getAllQuestions().then(response => response.json())
+            .then((response: Questions) => {
+                filteredQuestions = questions = response;
+                loading = false;
+                for (let question of questions.data) {
+                    let date = new Date(question.created_at);
+                    question.created_date = date.toDateString() + ' ' + date.getHours() + ':' + ('0' + date.getMinutes()).substr(-2);
+                }
+            }).catch((err) => {
             loading = false;
-            for (let question of questions.data) {
-                let date = new Date(question.created_at);
-                question.created_date = date.toDateString() + ' ' + date.getHours() + ':' + ('0' + date.getMinutes()).substr(-2);
-            }
-        }).catch((err) => {
-        loading = false;
-        error = 'Could not load the questions';
-    });
+            error = 'Could not load the questions';
+        });
+    }
+
+    getQuestions();
+
+    function filterQuestions() {
+        if (searchValue === '') {
+            getQuestions();
+        } else {
+            filteredQuestions.data = questions.data.filter(function (e) {
+                return e.title.toLowerCase().includes(searchValue.toLowerCase())
+                    || e.description.toLowerCase().includes(searchValue.toLowerCase());
+            });
+        }
+    }
 </script>
 
 {#if error}
@@ -36,28 +53,40 @@
 {#if questions && !error && !loading}
     <div class="questions">
         <h3>Most recent questions</h3>
-        {#each questions.data.sort((q1, q2) => q1.created_at < q2.created_at) as question}
-            <a href="/questions/{question.uuid}">
-                <div class="question-content">
-                    <div class="title-section">
-                        <p>{question.title}</p>
-                        <div class="written-by">
-                            <p>Written by {question?.user?.name ?? '--'} on {question?.created_date}</p>
+        <div class="search-bar">
+            <div class="input-field">
+                <input type="text" placeholder="Search for a question..." bind:value={searchValue}
+                       on:change={filterQuestions}>
+            </div>
+        </div>
+        {#if filteredQuestions.data.length === 0}
+            <div class="question-content">
+                <p>Could not find something for {searchValue}</p>
+            </div>
+        {:else}
+            {#each filteredQuestions.data.sort((q1, q2) => q1.created_at < q2.created_at) as question}
+                <a href="/questions/{question.uuid}">
+                    <div class="question-content">
+                        <div class="title-section">
+                            <p>{question.title}</p>
+                            <div class="written-by">
+                                <p>Written by {question?.user?.name ?? '--'} on {question?.created_date}</p>
+                            </div>
                         </div>
+                        <p id="description">
+                            { question?.description?.length > 250 ? question?.description.substring(0, 250 - 3) + '...' : question?.description }
+                        </p>
+                        {#if question.categories}
+                            <div class="categories">
+                                {#each question?.categories as category}
+                                    <p>{category.name}</p>
+                                {/each}
+                            </div>
+                        {/if}
                     </div>
-                    <p id="description">
-                        { question?.description?.length > 250 ? question?.description.substring(0, 250 - 3) + '...' : question?.description }
-                    </p>
-                    {#if question.categories}
-                        <div class="categories">
-                            {#each question?.categories as category}
-                                <p>{category.name}</p>
-                            {/each}
-                        </div>
-                    {/if}
-                </div>
-            </a>
-        {/each}
+                </a>
+            {/each}
+        {/if}
     </div>
 {/if}
 
@@ -65,6 +94,33 @@
   .questions {
     margin-left: 15%;
     margin-right: 15%;
+
+    .search-bar {
+      .input-field {
+        background-color: #f0f0f0;
+        margin: 10px 0;
+        height: 40px;
+        border-radius: 55px;
+        display: grid;
+        padding: 0 1rem;
+        position: relative;
+
+        input {
+          background: none;
+          outline: none;
+          border: none;
+          line-height: 1;
+          font-weight: 500;
+          font-size: 1rem;
+          color: #333;
+
+          &::placeholder {
+            color: #aaa;
+            font-weight: 500;
+          }
+        }
+      }
+    }
 
     a {
       color: inherit;
--- stackshare/src/routes/questions/index.svelte
+++ stackshare/src/routes/questions/index.svelte
@@ -1,3 +1,16 @@
 <script lang="ts">
 
 </script>
+
+<div class="details-content">
+    <p>Could not find a question, please search for another question</p>
+</div>
+
+
+<style lang="scss">
+  .details-content {
+    margin-top: 25px;
+    margin-left: 15%;
+    margin-right: 15%;
+  }
+</style>
