c7e0bb1f6cdf66a424703c7dda54cd566f4fa8a5
Louis D'Hont <xevrobelgium@gmail.com>
2022-01-18 22:51:56 +0100

    Update route-literals.model.ts

--- stackshare/src/models/core-models/route-literals.model.ts
+++ stackshare/src/models/core-models/route-literals.model.ts
@@ -1,6 +1,6 @@
 const production = false;
 export class RouteLiterals {
-    static baseUrl = (production) ? 'https://stackshare-api.ew.r.appspot.com' : 'http://localhost:5050';
+    static baseUrl = (production) ? 'https://stackshare-api.ew.r.appspot.com' : 'http://localhost:8080';
     static login = 'login';
     static register = 'register';
     static status = 'status';
